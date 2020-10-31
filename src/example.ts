9b8d871dd2f9ec6865f9e1383c16022c010f294e
Sergey Grivin <cerega32@mail.ru>
2020-10-31 15:32:40 +0300

    Create example.ts

---
+++ src/example.ts
@@ -0,0 +1,33 @@
+const concat = (str1: string, str2: string): string => {
+  return str1 + str2;
+}
+
+concat('Hello ', 'World');
+
+type TsArr = (number | string);
+
+type TsMyHometask = {
+  howIDoIt: string,
+  simeArray: TsArr[],
+  withData?: TsMyHometask[]
+}
+
+
+const MyHometask: TsMyHometask = {
+	howIDoIt: "I Do It Wel",
+	simeArray: ["string one", "string two", 42],
+	withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23]}],
+}
+
+
+
+interface MyArray<T> {
+	[N: number]: T;
+
+  reduce<U>(fn: (acc: U, el: T) => U, acc: U): U
+}
+
+
+const tsArr: MyArray<number> = [1,2,3,4]
+
+tsArr.reduce((acc, el) => acc + el, 0)
