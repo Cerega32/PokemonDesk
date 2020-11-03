a7c5b00c9bd8e57e06869bb38cf67b3e54bdce3e
Sergey Grivin <cerega32@mail.ru>
2020-11-03 19:34:17 +0300

    Added Header && Footer

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,14 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
         test: /\.scss$/,
         use: [
           'style-loader',
