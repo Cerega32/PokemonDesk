cbac70364acca9adf3578139f8b4256457401115
Sergey Grivin <cerega32@mail.ru>
2020-11-26 19:52:06 +0300

    Added page Pokemon and added tests

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -2,20 +2,27 @@ import React, { useState } from 'react';
 import Footer from '../../components/footer';
 import PokemonCard from '../../components/PokemonCard';
 import useData from '../../hook/getData';
+import useDebounce from '../../hook/useDebounce';
+import { IPokemons, PokemonsRequest } from '../../interface/pokemons';
 
 import s from './Pokedex.module.scss';
 
+interface IQuery {
+  name?: string;
+}
+
 const PokedexPage = () => {
   const [searchValue, setSearchValue] = useState('');
-  const [query, setQuery] = useState({});
+  const [query, setQuery] = useState<IQuery>({});
+  const debuncedValue = useDebounce(searchValue, 500);
 
-  const { data, isLoading, isError } = useData('getPokemons', query, [searchValue]);
+  const { data, isLoading, isError } = useData<IPokemons>('getPokemons', query, [debuncedValue]);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
     // eslint-disable-next-line no-shadow
-    setQuery((s) => ({
-      ...s,
+    setQuery((state: IQuery) => ({
+      ...state,
       name: e.target.value,
     }));
   };
@@ -35,7 +42,8 @@ const PokedexPage = () => {
           <input type="text" value={searchValue} onChange={handleSearchChange} />
         </div>
         {!isLoading &&
-          data.pokemons.map((pokemon) => (
+          data &&
+          data.pokemons.map((pokemon: PokemonsRequest) => (
             <PokemonCard
               name={pokemon.name_clean}
               attack={pokemon.stats.attack}
